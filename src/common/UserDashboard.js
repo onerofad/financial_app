98d1b77a403f7c710ee31b85897c0cc2ac33da5d
frank@gmail.com <imafidonfrank2015@gmail.com>
2024-01-24 18:57:37 +0100

    first commit

--- src/common/UserDashboard.js
+++ src/common/UserDashboard.js
@@ -105,9 +105,9 @@ export const UserDashboard = () => {
                                 </Table.Header>
                                 <Table.Body>
                                     <Table.Row>
-                                        <Table.Cell style={{fontSize: '0.5em'}}>{accountname}</Table.Cell>
-                                        <Table.Cell style={{fontSize: '0.5em'}}>{accountype}</Table.Cell>
-                                        <Table.Cell style={{fontSize: '0.5em'}}>{accountnumber}</Table.Cell>
+                                        <Table.Cell style={{fontSize: '0.9em'}}>{accountname}</Table.Cell>
+                                        <Table.Cell style={{fontSize: '0.9em'}}>{accountype}</Table.Cell>
+                                        <Table.Cell style={{fontSize: '0.9em'}}>{accountnumber}</Table.Cell>
                                     </Table.Row>
                                 </Table.Body>
 
